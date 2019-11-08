2203fb026ca192efad349a8f13878b08243c04ff
Joshua Skootsky <joshua.skootsky@gmail.com>
2019-11-08 15:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musics, instrums) {
+  var arr = [];
+  
+  for (var i = 0; i < musics.length; i++) {
+    var music = musics[i];
+    var instru = instrums[i];
+    
+    var phrase = music + " plays " + instru;
+    arr.push(phrase);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(arrFacts) {
+  var factCount = 0;
+  while( factCount < arrFacts.length) {
+    arrFacts[factCount] = arr
+    Facts[factCount] + "!!!";
+    factCount++;
+  }
+  return arrFacts;
+}
